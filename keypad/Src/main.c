8ae66e5320d5294f0a3043574471da41b1c6158b
Oleksandr Tympshenko <hermeusmanua@gmail.com>
2024-01-26 14:17:27 +0200

    improvement
    - deleted old commented code;
    - added delay in 250ms based on MCU specifications.

--- keypad/Src/main.c
+++ keypad/Src/main.c
@@ -1,78 +1,3 @@
-//#include <stdint.h>
-//#include <stdio.h>
-//#include "main.h"
-//
-//uint32_t volatile *const RCC_AHB1ENR = (uint32_t*)(0x40023800 + 0x30);
-//uint32_t volatile *const GPIOA_MODER = (uint32_t*)(0x40020000 + 0x00);
-//uint32_t volatile *const GPIOA_PUPDR = (uint32_t*)(0x40020000 + 0x0C);
-//uint32_t volatile *const GPIOA_IDR = (uint32_t*)(0x40020000 + 0x10);
-//uint32_t volatile *const GPIOA_ODR = (uint32_t*)(0x40020000 + 0x14);
-//
-//void read_keypad(char char1, char char2, char char3, char char4);
-//void set_pins_1(void);
-//void delay(void);
-//
-//int main(void) {
-//
-//	*RCC_AHB1ENR |= 1;
-//	*GPIOA_MODER &= ~(0xF); // set from PA0 to PA1 as input
-//	*GPIOA_MODER &= ~(0xF << 16); // set from PA8 to PA9 as input
-//	*GPIOA_MODER |= (0x55 << 8); // starting set from PA4 to PA7 as output
-//	*GPIOA_MODER &= ~(0xAA << 8); // ending set from PA4 to PA7 as output
-//	*GPIOA_PUPDR |= (0x5 << 0); // starting enabling from PA0 to PA1 pull-up resistor
-//	*GPIOA_PUPDR &= ~(0xA << 0); // ending enabling from PA0 to PA1 pull-up resistor
-//	*GPIOA_PUPDR |= (0x5 << 16); // starting enabling from PA8 to PA9 pull-up resistor
-//	*GPIOA_PUPDR &= ~(0xA << 16); // ending enabling from PA8 to PA9 pull-up resistor
-//
-//
-//	while(1) {
-//		set_pins_1();
-//		*GPIOA_ODR &= ~(1 << 4);
-//		read_keypad('1', '2', '3', 'A');
-//
-//		set_pins_1();
-//		*GPIOA_ODR &= ~(1 << 5);
-//		read_keypad('4', '5', '6', 'B');
-//
-//		set_pins_1();
-//		*GPIOA_ODR &= ~(1 << 6);
-//		read_keypad('7', '8', '9', 'C');
-//
-//		set_pins_1();
-//		*GPIOA_ODR &= ~(1 << 7);
-//		read_keypad('*', '0', '#', 'D');
-//	}
-//}
-//
-//void read_keypad(char char1, char char2, char char3, char char4) {
-//	if(!(((*GPIOA_IDR >> 0) & 1) | 0)) {
-//		delay();
-//		printf(" %c\n", char1);
-//		set_pins_1();
-//	} else if(!(((*GPIOA_IDR >> 1) & 1) | 0)) {
-//		delay();
-//		printf(" %c\n", char2);
-//		set_pins_1();
-//	} else if(!(((*GPIOA_IDR >> 8) & 1) | 0)) {
-//		delay();
-//		printf(" %c\n", char3);
-//		set_pins_1();
-//	} else if(!(((*GPIOA_IDR >> 9) & 1) | 0)) {
-//		delay();
-//		printf(" %c\n", char4);
-//		set_pins_1();
-//	}
-//}
-//
-//void set_pins_1(void) {
-//	*GPIOA_ODR |= 0xF << 4;
-//}
-//
-//void delay(void) {
-//	for(uint32_t volatile i = 0; i < 350000; i++);
-//}
-
-
 #include <stdint.h>
 #include <stdio.h>
 #include "main.h"
@@ -123,20 +48,20 @@ int main(void) {
 
 void read_keypad(char char1, char char2, char char3, char char4) {
 	if(!(GPIOA_IDR->IDR0)) {
-		delay();
 		printf(" %c\n", char1);
+		delay();
 		set_pins_1();
 	} else if(!(GPIOA_IDR->IDR1)) {
-		delay();
 		printf(" %c\n", char2);
+		delay();
 		set_pins_1();
 	} else if(!(GPIOA_IDR->IDR8)) {
-		delay();
 		printf(" %c\n", char3);
+		delay();
 		set_pins_1();
 	} else if(!(GPIOA_IDR->IDR9)) {
-		delay();
 		printf(" %c\n", char4);
+		delay();
 		set_pins_1();
 	}
 }
@@ -149,6 +74,25 @@ void set_pins_1(void) {
 }
 
 void delay(void) {
-	for(uint32_t volatile i = 0; i < 350000; i++);
+	for(uint32_t volatile i = 0; i < 571428; i++);
+	/* each iteration of the loop =
+	 execution of 7 processor assembly instructions:
+
+	1 ldr     r3, [r7, #4]
+	2 adds    r3, #1
+	3 str     r3, [r7, #4]
+	4 ldr     r3, [r7, #4]
+	5 ldr     r2, [pc, #16]   ; (0x8000580 <delay+40>)
+	6 cmp     r3, r2
+	7 bls.n   0x8000564 <delay+12>
+
+	processor running with frequency 16MHz of internal RC oscillator (by default)
+
+	-> time for 6 instructions to be executed = 7 / 16 (10^6) = 4,375 * (10^-7)
+	-> number of loop iterations for 1 second delay = 2 285 714 (iter's)
+	-> number of loop iterations for 0.5 second delay = 1 142 857 (iter's)
+	-> number of loop iterations for 0.250 second delay = 571 428 (iter's)
+
+	 */
 }
 
